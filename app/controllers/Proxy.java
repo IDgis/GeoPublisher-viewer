5d7816576c0c2a494cf43b214e25062de17317b2
Sandro Neumann <sandro.neumann@idgis.nl>
2015-07-28 16:49:30 +0200

    Added queryparameter loops to proxy

--- app/controllers/Proxy.java
+++ app/controllers/Proxy.java
@@ -1,12 +1,35 @@
 package controllers;
 
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import play.libs.F.Promise;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Proxy extends Controller {
-
-	public Result proxy(String url) {
-		return ok ("Verwerkt: " + url).as ("text/html");
+	
+	@Inject WSClient ws;
+	
+	public Promise<Result> proxy(String url) {
+		String completeUrl = "http://staging-services.geodataoverijssel.nl/" + url;
+		WSRequest request = ws.url(completeUrl).setFollowRedirects(true);
+		
+		Map<String, String[]> colStr = request().queryString();
+		
+		for (Map.Entry<String, String[]> entry: colStr.entrySet()) {
+			for(String entryValue: entry.getValue()) {
+				request.setQueryParameter(entry.getKey(), entryValue);
+			}
+		}
+		
+		Promise<WSResponse> responsePromise = request.get();
+		Promise<Result> resultPromise = responsePromise.map (response -> ok(response.asByteArray()).as(response.getHeader(CONTENT_TYPE)));
+		
+		return resultPromise;
 	}
-
 }
